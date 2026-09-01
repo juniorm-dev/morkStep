c5b8be5c1ad42189241a0eff1aec2d16d069aa18
junior <juniorm33@hotmail.com>
2026-09-01 01:28:55 +0100

    Bump versions: app 0.6.0->0.7.0 (code 8), wear 0.3.0->0.4.0 (code 4)

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "com.morkstep.wear"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     testOptions {
